76aa08fd167081760a7a774d6caf6c2e5804c63e
thrice <thricenope@gmail.com>
2021-11-27 23:07:44 +0800

    111

---
+++ src/views/login/login.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="wrapper">
+    <img
+      src="http://www.dell-lee.com/imgs/vue3/user.png"
+      class="wrapper__img"
+    >
+    <div class="wrapper__input">
+      <input
+        class="wrapper__input__content"
+        placeholder="请输入用户名"
+        v-model="data.username">
+    </div>
+    <div class="wrapper__input">
+      <input
+        class="wrapper__input__content"
+        placeholder="请输入密码"
+        type="password"
+        v-model="data.password">
+    </div>
+    <div class="wrapper__login-button" @click="handleLogin">登录</div>
+    <div class="wrapper__login-link" @click="handleRegisterClick">立即注册</div>
+  </div>
+</template>
+<script>
+import { useRouter } from 'vue-router'
+import { post } from '../../../src/utils/request'
+import { reactive } from 'vue'
+
+// axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+export default {
+  name: 'Login',
+  setup () {
+    const data = reactive({
+      username: '',
+      password: ''
+    })
+    const router = useRouter()
+    const handleLogin = async () => {
+      try {
+        const result = await post('/api/user/login', {
+          username: data.username,
+          password: data.password
+        })
+        console.log(result)
+        if (result?.errno === 0) {
+          localStorage.isLogin = true
+          await router.push({ name: 'Home' })
+        } else {
+          alert('login fail')
+        }
+      } catch (e) {
+        alert('send fail')
+      }
+    }
+    const handleRegisterClick = () => {
+      router.push({ name: 'Register' })
+    }
+    return {
+      handleLogin,
+      handleRegisterClick,
+      data
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "../../style/variable";
+
+.wrapper {
+  position: absolute;
+  top: 50%;
+  left: 0;
+  right: 0;
+  transform: translateY(-50%);
+
+  &__img {
+    display: block;
+    margin: 0 auto .4rem auto;
+    width: .66rem;
+    height: .66rem;
+  }
+
+  &__input {
+    height: .48rem;
+    margin: 0 .4rem .16rem .4rem;
+    background: #F9F9F9;
+    border: 1px solid rgba(0, 0, 0, 0.10);
+    border-radius: 6px;
+
+    &__content {
+      box-sizing: border-box;
+      width: 100%;
+      line-height: .48rem;
+      border: none;
+      outline: none;
+      background: none;
+      padding: 0 .16rem;
+      font-size: .16rem;
+      color: $content-notice-fontcolor;
+
+      &::placeholder {
+        color: $content-notice-fontcolor;
+      }
+    }
+  }
+
+  &__login-button {
+    line-height: .48rem;
+    margin: .32rem .4rem .16rem .4rem;
+    background: #0091FF;
+    box-shadow: 0 .04rem .08rem 0 rgba(0, 145, 225, 0.32);
+    border-radius: .04rem;
+    color: #fff;
+    font-size: .16rem;
+    text-align: center;
+  }
+
+  &__login-link {
+    text-align: center;
+    font-size: .14rem;
+    color: $content-notice-fontcolor;
+  }
+}
+</style>
