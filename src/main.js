2f38938478e9ad826af8011c5af44424517c11f9
thricenope <thricenope@gmail.com>
2021-11-28 14:18:21 +0800

    git jd首页不完整提交

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 import 'normalize.css'
 import './style/base.scss'
+import './style/index.scss'
 
 createApp(App).use(store).use(router).mount('#app')
